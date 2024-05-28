342cf695f5f9828d45ca8ce66e9b1e2f0c4b186c
ZhovtenkoA <zhowtenko.oleksiy@gmail.com>
2024-05-28 12:22:17 +0300

    add lesson 8

---
+++ Lesson8/lesson8.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+
+
+class Contacts {
+public:
+	std::string* contacts_list;
+	int size;
+	int index = 0;
+	Contacts(int size) {
+		this->size = size;
+		contacts_list = new std::string[size];
+	}
+
+	void add_contact(std::string number) {
+		contacts_list[index] = number;
+		index++;
+
+
+	}
+
+	void view_contacts() {
+
+		for (int i = 0; i < index; i++)
+			std::cout <<"Contact" << i<<": " << contacts_list[i] << "\n";
+	}
+
+	~Contacts() {
+		delete[] contacts_list;
+	}
+};
+
+
+
+
+
+
+
+int main() {
+	SetConsoleOutputCP(1251);
+	/*
+	Contacts contact(5);
+	contact.add_contact("009999954");
+	contact.add_contact("00988849954");
+	contact.view_contacts();
+	*/
+
+
+
+}
